780ff40bc8bfcdf700b0d3cd03ee5e9727d21b60
Muhammed Firos <106723065+Firosvpz@users.noreply.github.com>
2024-11-06 14:49:57 +0530

    Update userAuth.ts

--- src/middlewares/userAuth.ts
+++ src/middlewares/userAuth.ts
@@ -7,7 +7,8 @@ interface AuthRequest extends Request {
 
 const userAuth = async (req: AuthRequest, res: Response, next: NextFunction): Promise<any> => {
   
- const token = req.header('x-auth-token');
+ const authHeader = req.header('Authorization'); // Use 'Authorization' header
+  const token = authHeader && authHeader.split(' ')[1]; // Extract the token after 'Bearer '
   
   if (!token)
     return res
